cd76a8ccfc27719389d64a2db3bc97f6e81ad523
ProjectS <issahakim94@gmail.com>
2021-02-04 12:40:17 +0100

    Added licenses and categories support

--- src/main/java/fr/minemobs/modrinthjavapi/get/GetTags.java
+++ src/main/java/fr/minemobs/modrinthjavapi/get/GetTags.java
@@ -5,6 +5,7 @@ import fr.minemobs.modrinthjavapi.MainClass;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.HashMap;
 
 public class GetTags {
 
@@ -23,6 +24,28 @@ public class GetTags {
         InputStreamReader inputStreamReader = null;
         try{
             URL url = new URL(MainClass.baseUrl + "tag/game_version");
+            inputStreamReader = new InputStreamReader(url.openStream());
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        return MainClass.getGson().fromJson(inputStreamReader, String[].class);
+    }
+
+    public static HashMap<String, String>[] getLicenses(){
+        InputStreamReader inputStreamReader = null;
+        try{
+            URL url = new URL(MainClass.baseUrl + "tag/license");
+            inputStreamReader = new InputStreamReader(url.openStream());
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        return MainClass.getGson().fromJson(inputStreamReader, HashMap[].class);
+    }
+
+    public static String[] getCategories(){
+        InputStreamReader inputStreamReader = null;
+        try{
+            URL url = new URL(MainClass.baseUrl + "tag/category");
             inputStreamReader = new InputStreamReader(url.openStream());
         }catch (IOException e){
             e.printStackTrace();
